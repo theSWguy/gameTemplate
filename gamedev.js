c3e7c4cd45d41f8da4334d20c81f6855737f478f
theweponyrebel <s200352@Jacobs-MacBook-Air.local>
2015-12-09 09:44:35 +0800

    Revert "Changed home image"
    This reverts commit f9592efedde0cf7a5f50b73be5fec777d4de2b42.

--- gamedev.js
+++ gamedev.js
@@ -53,7 +53,7 @@ $( document ).ready(function() {
 
 
 	$("#goBack").click(function() {
-		$("#exploreBody").css("background-image", "url('img/88rLnlH.jpg')");
+		$("#exploreBody").css("background-image", "url('img/exploreHome1.jpg')");
 		$("#goBack").hide();
 		$("#goRight1").show();
 		$("#goLeft1").show();
